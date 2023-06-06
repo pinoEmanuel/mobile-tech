d1e51ce1a15c8fce02abe1219edc315d14f2322b
Emanuel Pino <emanuelpino42@gmail.com>
2023-06-06 12:05:15 -0300

    correcion de errores

--- js/index.js
+++ js/index.js
@@ -18,6 +18,35 @@ const celulares =  [{id: 1, nombre: "Apple", monto: 160000, stock: 200},
                     {id: 4, nombre: "Xiaomi", monto: 125000, stock: 200},
                     {id: 5, nombre: "TCL", monto: 126000, stock: 200}]
 
+function buscarCelular(id){
+    let resultado = celulares.find((celular)=> celular.id === parseInt(id))
+    return resultado
+}
+
+function calcularYMostrarCuotaMensual(monto, meses, interes){
+    console.log("--- DETALLE DE LAS CUOTAS ---")
+    let calculoCuotas = (monto * interes) / meses
+    for (let i = 1; i <=  meses; i++){
+        console.log("Cuota ", i, "AR$ ", calculoCuotas.toFixed(2))
+    }
+}
+
+function generarCarrito(){
+    const shop = new Compra(carrito)
+    subTotal = shop.obtenerSubTotal()
+    alert("El costo total de su compra es de: $" + subTotal + ".")
+}
+
+function mostrarCarrito(){
+    const arrayCarrito = carrito.map((celular) => {
+        return {
+            nombre: celular.nombre,
+            monto: celular.monto
+        }
+    })
+    console.log("--- CARRITO DE COMPRAS ---")
+    console.table(arrayCarrito)
+}
 
 function inicio(){
     alert("Hola! \nBienvenido a nuestra tienda de celulares.")
@@ -34,11 +63,6 @@ function mostrarCelulares(){
     })
     console.table(arrayCelulares)
     elegirCelular()   
-}
-
-function buscarCelular(id){
-    let resultado = celulares.find((celular)=> celular.id === parseInt(id))
-    return resultado
 }
 
 function elegirCelular(){
@@ -60,55 +84,31 @@ function elegirCelular(){
             }
         }
         else{
-            mostrarCelulares()
+            elegirCelular()
             }
     }
     else{
         alert("El codigo ingresado es incorrecto. Por favor intenta con un código válido.")
-        mostrarCelulares()
+        elegirCelular()
         }
     }
-
-function generarCarrito(){
-    const shop = new Compra(carrito)
-    subTotal = shop.obtenerSubTotal()
-    alert("El costo total de su compra es de: $" + subTotal + ".")
-}
 
 function elegirCuotas(subTotal){
     let cantidadCuotas = prompt("¿En cuántas cuotas deseas pagar tu smartphone? (Cantidad mínima: 1, Cantidad máxima: 48)")
     if (isNaN(cantidadCuotas)){
         console.error("Ingrese un valor numérico, por favor.")
-        elegirCuotas()
+        elegirCuotas(subTotal)
     }
     if (cantidadCuotas > 0 && cantidadCuotas <= 48){
-        alert("Su elección ha sido guardada correctamente. \nA continuación se mostrarán los detalles de la compra en " + cantidadCuotas + " cuotas y su carrito respectivamente.\n¡Muchas gracias por tu compra!")
+        alert("La elección ha sido guardada correctamente. \nA continuación se mostrarán los detalles de la compra en " + cantidadCuotas + " cuotas y su carrito respectivamente.\n¡Muchas gracias por tu compra!")
         mostrarCarrito()
         calcularYMostrarCuotaMensual(subTotal, cantidadCuotas, interesCompra)
 
     }
     else {
         console.warn("La cantidad de cuotas no es válida, por favor ingresa nuevamente un valor.")
-        elegirCuotas()
+        elegirCuotas(subTotal)
     }
 }
 
-function calcularYMostrarCuotaMensual(monto, meses, interes){
-    console.log("--- DETALLE DE LAS CUOTAS ---")
-    let calculoCuotas = (monto * interes) / meses
-    for (let i = 1; i <=  meses; i++){
-        console.log("Cuota ", i, "AR$ ", calculoCuotas.toFixed(2))
-    }
-}
-
-function mostrarCarrito(){
-    const arrayCarrito = carrito.map((celular) => {
-        return {
-            nombre: celular.nombre,
-            monto: celular.monto
-        }
-    })
-    console.table(arrayCarrito)
-}
-
-//inicio()
+inicio()
